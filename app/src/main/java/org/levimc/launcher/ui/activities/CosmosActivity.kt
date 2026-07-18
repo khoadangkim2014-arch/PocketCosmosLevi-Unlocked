d4c859e24345632688bd78f262d5646dafd7587d
strawberry9120 <vedefraco110@gmail.com>
2026-07-18 23:24:39 +0200

    changed cosmos version

--- app/src/main/java/org/levimc/launcher/ui/activities/CosmosActivity.kt
+++ app/src/main/java/org/levimc/launcher/ui/activities/CosmosActivity.kt
@@ -21,7 +21,7 @@ class CosmosActivity : BaseActivity() {
     private lateinit var manager: InbuiltModManager
 
     companion object {
-        const val COSMOS_VERSION = "1.1.0"
+        const val COSMOS_VERSION = "1.1.1"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
