cd9f0b313bfc3659bcc6c895b19a8541b09c6183
khoadangkim2014-arch <khoadangkim2014@gmail.com>
2026-09-13 17:30:35 +0700

    Clean up PlayStoreValidator by removing unused code
    Removed unused imports and constants from PlayStoreValidator.

--- app/src/main/java/org/levimc/launcher/util/PlayStoreValidator.java
+++ app/src/main/java/org/levimc/launcher/util/PlayStoreValidator.java
@@ -1,13 +1,8 @@
 package org.levimc.launcher.util;
 
 import android.content.Context;
-import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.os.Build;
 
 public class PlayStoreValidator {
-    private static final String MINECRAFT_PACKAGE_NAME = "com.mojang.minecraftpe";
-    private static final String PLAY_STORE_INSTALLER = "com.android.vending";
 
     public static boolean isMinecraftFromPlayStore(Context context) {
         return true;
@@ -20,5 +15,4 @@ public class PlayStoreValidator {
     public static boolean isLicenseVerified(Context context) {
         return true;
     }
-
 }
